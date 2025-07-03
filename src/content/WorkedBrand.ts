48484e488acd76be13621769b98b9c00a180d2f8
sakib <sahadothosain6619@gmail.com>
2025-07-03 23:31:32 +0600

    update rating daynamic

---
+++ src/content/WorkedBrand.ts
@@ -0,0 +1,37 @@
+export const WorkedBrand = [
+  { name: "Dream Diver It", logo: "/images/company/DreamDiver.png" },
+  { name: "Fiverr", logo: "/images/company/fiverr.png" },
+  { name: "upwork", logo: "/images/company/upwork.png" },
+  { name: "quantum", logo: "/images/company/quamtum.png" },
+  { name: "Misi", logo: "/images/company/misi.png" },
+  { name: "Foody Moody", logo: "/images/company/fodymody.png" },
+  { name: "roterdam", logo: "/images/company/roterdam.svg" },
+  {
+    name: "deshi",
+    logo: "/images/company/deshidistributors_logo.png",
+  },
+  {
+    name: "relible",
+    logo: "/images/company/relible_logo.png",
+  },
+];
+export const Reviews = [
+  {
+    rating: 5,
+    comment: "Great work, very professional and fast delivery.",
+    name: "John Doe",
+    company: "Tech Solutions",
+  },
+  {
+    rating: 5,
+    comment: "Great work, very professional and fast delivery.",
+    name: "John Doe",
+    company: "Tech Solutions",
+  },
+  {
+    rating: 5,
+    comment: "Great work, very professional and fast delivery.",
+    name: "John Doe",
+    company: "Tech Solutions",
+  },
+];
