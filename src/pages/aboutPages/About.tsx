ff973f0541c15124749f522d3ad4c9c82c8c11cb
sakib <sahadothosain6619@gmail.com>
2025-07-17 22:10:48 +0600

    completed all

--- src/pages/aboutPages/About.tsx
+++ src/pages/aboutPages/About.tsx
@@ -58,7 +58,7 @@ const About = () => {
                         />
                         <img
                           className="circle-image circle-image-light d-none"
-                          src="assets/img/about-us/circle-text-light.svg"
+                          src="/images/icons/circle-text-light.svg"
                           alt="circle-text"
                         />
                         <span className="arrow-down">
@@ -96,21 +96,6 @@ const About = () => {
               </div>
             </div>
           </div>
-        </div>
-      </div>
-
-      <div className="background-shapes">
-        <div className="shape-1 common-shape">
-          <img src="assets/img/bg/banner-shape-1.png" alt="banner-shape-1" />
-        </div>
-        <div className="shape-2 common-shape">
-          <img src="assets/img/bg/banner-shape-1.png" alt="banner-shape-1" />
-        </div>
-        <div className="threed-shape-1 move-with-cursor" data-value="1">
-          <img src="assets/img/bg/object-3d-1.png" alt="object-3d-1" />
-        </div>
-        <div className="threed-shape-2 move-with-cursor" data-value="1">
-          <img src="assets/img/bg/object-3d-2.png" alt="object-3d-2" />
         </div>
       </div>
     </>
